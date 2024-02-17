a6b11960f99809413ec4bc430ad9dff21c4c826f
tanuki <tanuki.evil@yandex.ru>
2024-02-17 21:32:48 +0300

    fixes

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -4,20 +4,6 @@ from random import randint, choice
 
 QUESTION_TEXT = "What is the result of the expression?"
 
-<<<<<<< Updated upstream
-def get_exp(num1, num2):
-    operators = [('+', operator.add), ('*', operator.mul), ('-', operator.sub)]
-    op_text, op = choice(operators)
-    return op_text, str(op(num1, num2))
-
-
-def get_data() -> tuple:
-    question_text = "What is the result of the expression?"
-    num1 = randint(1, 50)
-    num2 = randint(1, 10)
-
-    op_text, answer = get_exp(num1, num2)
-=======
 
 def get_exp() -> tuple:
     operators = [('+', operator.add), ('*', operator.mul), ('-', operator.sub)]
@@ -29,6 +15,5 @@ def get_exp() -> tuple:
 
 def get_data() -> tuple:
     num1, num2, op_text, answer = get_exp()
->>>>>>> Stashed changes
     question_exp = f'{num1} {op_text} {num2}'
     return QUESTION_TEXT, question_exp, answer
--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -22,15 +22,7 @@ def get_answer() -> tuple:
 
 
 def get_data() -> tuple:
-<<<<<<< Updated upstream
-    question_text = 'Answer "yes" if given number is prime. ' \
-                    'Otherwise answer "no".'
-    num1 = randint(1, 100)
-    answer = 'yes' if is_prime(num1) else 'no'
-    question_exp = str(num1)
-    return question_text, question_exp, answer
-=======
     num, answer = get_answer()
     question_exp = str(num)
     return QUESTION_TEXT, question_exp, answer
->>>>>>> Stashed changes
+
